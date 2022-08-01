4e5213509de28be8dcb3cc36261f07232054450a
Sidia <sidia.reis1@gmail.com>
2022-07-31 20:10:20 -0700

    feat: Init conversion return type to Gson

--- app/src/main/java/br/com/alura/inventory/retrofit/InventoryRetrofit.java
+++ app/src/main/java/br/com/alura/inventory/retrofit/InventoryRetrofit.java
@@ -2,6 +2,7 @@ package br.com.alura.inventory.retrofit;
 
 import br.com.alura.inventory.retrofit.service.ProductService;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class InventoryRetrofit {
 
@@ -10,6 +11,7 @@ public class InventoryRetrofit {
     public InventoryRetrofit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://192.168.20.249:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
          productService = retrofit.create(ProductService.class);
     }
